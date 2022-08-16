6a0ae4febc5f8a6b4ba02a2719ac9baae32a7826
Rejoice <uahomorejoice@gmail.com>
2022-08-16 01:00:05 +0100

    added middilewares

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const userRoute = require("./routes/userRoute");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
