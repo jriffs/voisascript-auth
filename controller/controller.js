f671f3bab1739c0cf5ab174645d11450f3d79941
Olatunde Amos <98148042+akoredetunde@users.noreply.github.com>
2022-09-24 00:43:58 +0100

    import and export the validateUserController file

--- controller/controller.js
+++ controller/controller.js
@@ -2,10 +2,11 @@ import updateController from "./updateController.js";
 import signupController from "./signupController.js";
 import getAllUserController from "./getAllUserController.js";
 import deleteUserController from "./deleteUserController.js";
-
+import validateUserController from "./validateUserController.js"
 export {
   signupController,
   updateController,
   getAllUserController,
   deleteUserController,
+  validateUserController,
 };
