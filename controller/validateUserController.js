1a6afaa51fb49c76c3dd4fae68051ea14c411698
Olatunde Amos <98148042+akoredetunde@users.noreply.github.com>
2022-09-27 10:47:15 +0100

    little update

--- controller/validateUserController.js
+++ controller/validateUserController.js
@@ -6,19 +6,42 @@ config();
 const validateUserController = (req, res) =>{
   const authorization = req.headers["authorization"].split(" ");
   jwt.verify(authorization[1], process.env.JWT_KEY, (err, decoded) => {
-    getOne(decoded.userID, (err, result) =>{
-      if(result){
-        if(req.headers["originator"] === "extension"){
-          res.status(200).json({status: true, result: result});
-        }else{
-          if(req.headers["originator"] === "file-storage"){
-            res.status(200).json({status: true, result: result});
+    if(decoded){
+      getOne(decoded.userID, (err, result) =>{
+        if (result) {
+          switch (req.headers["originator"]) {
+            case "extension":
+              getUserData("projects", decoded.username).then(data =>{
+                res.status(200).
+                json({
+                  isUser: true,
+                  userData: data
+                })
+              })
+              break;
+            case "file-storage":
+              res.status(200).
+              json({
+                isUser: true,
+                userData: {
+                  username: decoded.username,
+                  userID: decoded.ID
+                }
+              });
+              break;
+            default:
+
           }
+        }else{
+          res.status(400).json({status: false, message: "User does not exist"});
         }
-      }else{
-        res.status(400).json({status: false, message: "User does not exist"})
-      }
-    })
+      })
+    }else{
+      res.json({
+        error: err
+      })
+    }
+
   })
 }
 
