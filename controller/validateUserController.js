5b81e8fcc481fdd7fae3c9e525aa97b71d7a5b55
Olatunde Amos <98148042+akoredetunde@users.noreply.github.com>
2022-09-27 12:24:47 +0100

    Some refactoring

--- controller/validateUserController.js
+++ controller/validateUserController.js
@@ -5,44 +5,39 @@ config();
 
 const validateUserController = (req, res) =>{
   const authorization = req.headers["authorization"].split(" ");
-  jwt.verify(authorization[1], process.env.JWT_KEY, (err, decoded) => {
-    if(decoded){
-      getOne(decoded.userID, (err, result) =>{
-        if (result) {
-          switch (req.headers["originator"]) {
-            case "extension":
-              getUserData("projects", decoded.username).then(data =>{
+  const payload = jwt.verify(authorization[1], process.env.JWT_KEY);
+    if(!payload.data.id){
+      res.sendstatus(401);
+    }else{
+      const {id, password, username} = payload.data;
+        getOne(id, (err, result) =>{
+          if (result) {
+            switch (req.headers["originator"]) {
+              case "extension":
+                getUserData("projects", username).then(data =>{
+                  res.status(200).
+                  json({
+                    isUser: true,
+                    userData: data
+                  });
+                });
+                break;
+              case "file-storage":
                 res.status(200).
                 json({
                   isUser: true,
-                  userData: data
-                })
-              })
-              break;
-            case "file-storage":
-              res.status(200).
-              json({
-                isUser: true,
-                userData: {
-                  username: decoded.username,
-                  userID: decoded.ID
-                }
-              });
-              break;
-            default:
+                  userData: {
+                    username: username,
+                    userID: id
+                  }
+                });
+                break;
+              default:
 
+            }
+          }else{
+            res.status(400).json({status: false, message: "User does not exist"});
           }
-        }else{
-          res.status(400).json({status: false, message: "User does not exist"});
-        }
-      })
-    }else{
-      res.json({
-        error: err
-      })
-    }
-
-  })
-}
+        })
 
 export default validateUserController;
