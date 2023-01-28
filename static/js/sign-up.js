a6a206c13717e97a1b6c9083e3bd3c422b1453c6
jriffs <mayjriffs97@gmail.com>
2023-01-28 17:53:10 +0100

    added hide password button

--- static/js/sign-up.js
+++ static/js/sign-up.js
@@ -5,7 +5,10 @@ import { notify } from "./notify.js";
 const body = document.body
 const sign_up_button = document.querySelector("#sign-up")
 const inputs = document.querySelectorAll("input")
+const showPass = document.querySelector("#show-pass")
+const pass_input = document.querySelector("#pass")
 sign_up_button.addEventListener("click", signUp)
+showPass.addEventListener("click", showPasswd)
 
 async function signUp(e) {
     e.preventDefault()
@@ -54,4 +57,17 @@ function validate() {
         if (i.checkValidity() == false) return false
     }
     return true
+}
+
+function showPasswd() {
+    if (pass_input.getAttribute("type") == "password") {
+        pass_input.setAttribute("type", "text")
+        showPass.innerHTML = ``
+        showPass.innerHTML = `<img src="./icons/icons8-closed-eye-24.png" alt="">`
+        return
+    }
+    pass_input.setAttribute("type", "password")
+    showPass.innerHTML = ``
+    showPass.innerHTML = `<img src="./icons/icons8-eye-30.png" alt="eye">`
+    return
 }
